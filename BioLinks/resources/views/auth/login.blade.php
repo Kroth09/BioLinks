2b4ec1362380ca97d76644df0b721c15c9103622
Kroth09 <eduardokroth10@gmail.com>
2026-04-02 11:47:54 -0300

    Feat: adicionando tela de login

---
+++ BioLinks/resources/views/auth/login.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <h1>Login</h1>
+
+    <form action="/login" method="post">
+        @csrf
+
+        <input name="email" placeholder="Email" value="{{old('email')}}"/>
+        <input name="password" placeholder="Senha"/>
+
+        <button>Logar</button>
+
+
+    </form>
+</div>
